ae80de69c4c4170535a7ecc677f6d1b6bcc19ab6
ssspiderzzz <ssspiderzzz@gmail.com>
2019-06-28 15:32:49 -0700

    done!

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+let sumItems = function(array) {
+  while (array.length > 0) {
+    for (let i = 0; i < array.lentgh; ++i) {
+      if (array[i] === undefined) {
+        array.slice(i,1);
+      }
+    }
+    const element = array.shift();
+
+    if (Array.isArray(element)) {
+      return sumItems(element) + sumItems(array);
+    } else {
+      return element + sumItems(array);
+    }
+  }
+  return 0;
+};
 
 module.exports = sumItems;
